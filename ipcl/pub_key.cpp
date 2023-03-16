57ce1a4976a93e2229c01a3c59ba8cd492d96b63
cgair <chengeaie23@sjtu.edu.cn>
2023-03-16 19:19:35 +0000

    Fix altered hs(maybe).

--- ipcl/pub_key.cpp
+++ ipcl/pub_key.cpp
@@ -194,7 +194,7 @@ void PublicKey::create(const BigNumber& n, int bits, const BigNumber& hs,
                        int randbits) {
   create(n, bits, false);  // set DJN to false and manually set
   m_enable_DJN = true;
-  m_hs = hs;
+  // m_hs = hs;
   m_randbits = randbits;
 }
 
